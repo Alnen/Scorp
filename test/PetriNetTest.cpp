1ead21d0f7cf6026e050cf0763271f4226151aa8
Alex Uzhegov <uzhegov37@yahoo.com>
2016-04-10 02:37:04 +0300

    Added test target

---
+++ test/PetriNetTest.cpp
@@ -0,0 +1,60 @@
+#include "gtest/gtest.h"
+#include "container/PetriNet.h"
+#include "container/TypeList.h"
+#include "container/IntegralIdGenerator.h"
+#include <memory>
+
+using TestMarkerList = TypeList<int>;
+using TestStateList = TypeList<int>;
+using TestTransitionList = TypeList<int>;
+
+template <>
+struct PetriNetTraits<TestMarkerList, TestStateList, TestTransitionList>
+{
+    using MarkerList = TestMarkerList;
+    using TransitionList = TestTransitionList;
+    using StateList = TestStateList;
+    using IdType = int;
+    using IdGenerator = IntegralIdGenerator<IdType>;
+};
+
+using TestPetriNetTrait = PetriNetTraits<TestMarkerList, TestStateList, TestTransitionList>;
+using TestPetriNet = PetriNet<TestPetriNetTrait>;
+
+class BasePetriNetTest: public testing::Test
+{
+public:
+    using IdType = TestPetriNetTrait::IdType;
+
+    virtual void SetUp() override {
+        m_petriNet = std::unique_ptr<TestPetriNet>(new TestPetriNet());
+    }
+
+    virtual void TearDown() override {
+        m_petriNet.reset();
+    }
+
+    std::unique_ptr<TestPetriNet> m_petriNet;
+};
+
+
+class PetriNetStateOperationsTest: public BasePetriNetTest
+{
+
+};
+
+TEST_F(PetriNetStateOperationsTest, ifPassedIntStateShouldSuccessfullyInsertStateAndReturnZeroId)
+{
+    IdType stateId = -1;
+    ASSERT_NO_THROW({ stateId = m_petriNet->addState<int>(10); });
+    ASSERT_EQ(0, stateId);
+}
+
+TEST_F(PetriNetStateOperationsTest, ifInsertSeveralStatesShouldReturnDifferentIds)
+{
+    IdType state1Id = -1;
+    IdType state2Id = -1;
+    ASSERT_NO_THROW({ state1Id = m_petriNet->addState<int>(10); });
+    ASSERT_NO_THROW({ state2Id = m_petriNet->addState<int>(20); });
+    ASSERT_NE(state1Id, state2Id);
+}
